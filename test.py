ba6fc0342cc3a57b332e260f5ad17f159df4c2e0
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2023-10-19 23:15:43 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+from github import Github
+import os
+
+# Authenticate with your personal access token
+g = Github(os.environ["ACCESS_TOKEN"])
+
+# Get the repository
+repo = g.get_repo("Abhijith14/Project-SmartGitAuto-v2")
+
+# Get the secrets
+secrets = repo.get_secrets()
+
+# Print the secrets and their values
+for secret in secrets:
+    print(secret.name, "=", secret.value)
